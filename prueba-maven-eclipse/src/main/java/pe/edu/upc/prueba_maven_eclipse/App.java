dca244f06b1031df3f0c293cfc65610c58fa9d60
Humberto Luis <krieg_1994@hotmail.com>
2016-12-12 13:15:26 -0500

    Primer version

---
+++ prueba-maven-eclipse/src/main/java/pe/edu/upc/prueba_maven_eclipse/App.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.prueba_maven_eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
